1de072cac86a09a1d90720620cb33678e1cfe03c
00nadia <nadia_tyb@poczta.fm>
2024-08-12 15:40:12 +0200

    update

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -1,8 +1,12 @@
 import { env } from '$env/dynamic/private';
+import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
-export const load = async ({fetch})=>{
+export const load: PageServerLoad = async ()=>{
     const response = await fetch(env.SERVER_URL);
+    
     const dataTest = await response.json();
+
     return{
         dataTest: dataTest,
     };
