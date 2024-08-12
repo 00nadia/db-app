9bf247a5b28d7cb11f7f5960ebaead2e0e9a7410
00nadia <nadia_tyb@poczta.fm>
2024-08-12 09:14:42 +0200

    adding docker

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 export const load = async (serverLoadEvent)=>{
     const {fetch}=serverLoadEvent;
-    const response = await fetch('http://localhost:4000/show');
+    const response = await fetch('http://host.docker.internal:4000/show');
     const dataTest = await response.json();
     return{
         dataTest: dataTest,
