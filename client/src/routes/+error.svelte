8f6daba0b9cf5f7bb589afd70ff3ee93ba16da52
00nadia <nadia_tyb@poczta.fm>
2024-08-13 10:50:11 +0200

    error

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<div class="bg-indigo-900 h-screen content-center text-center w-screen text-white">
+  <h1 class="text-4xl">{$page.status} {$page.error.message}</h1>
+  <span class="text-2xl"></span>
+</div>
--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -4,10 +4,19 @@ import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ()=>{
     const response = await fetch(env.SERVER_URL);
+    if (response.status === 200){
+        const dataTest = await response.json();
+        if (dataTest.includes){
+            return{
+                dataTest: dataTest,
+            }
+        }else{
+                throw error(response.status ,response.statusText);
+        }
+    }
+    else{
+        throw error(response.status ,response.statusText);
+    }
     
-    const dataTest = await response.json();
 
-    return{
-        dataTest: dataTest,
-    };
 };
